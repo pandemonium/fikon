eca70017f6ced93307abf51591f59f0dc445ec6e
Patrik Andersson <pandersson@gmail.com>
2024-01-19 11:04:39 +0100

    Adds more combinators. Vanity formatting. Bugfixes.

--- Combinatrix.Tests/Tests.fs
+++ Combinatrix.Tests/Tests.fs
@@ -8,37 +8,130 @@ open Combinatrix.Combine
 
 [<Fact>]
 let ``String literals`` () =
-    let p = Parser.Text.literal "Hi, mom"
-    let outcome = Parser.Text.run p "Hi, mom. What are you doing?"
+    let p = Parse.Text.literal "Hi, mom"
+    let outcome = Parse.Text.run p "Hi, mom. What are you doing?"
     Assert.Equal(Some "Hi, mom", outcome.Returns)
 
 [<Fact>]
 let ``Sequencing`` () =
-    let p = Parser.Text.literal "Hi, mom."
-    let q = Parser.Text.literal " What are you doing?"
-    let parser = Parser.andAlso p q
-    let outcome = Parser.Text.run parser "Hi, mom. What are you doing?"
+    let p = Parse.Text.literal "Hi, mom."
+    let q = Parse.Text.literal " What are you doing?"
+    let parser = Parse.andAlso p q
+    let outcome = Parse.Text.run parser "Hi, mom. What are you doing?"
     Assert.Equal(Some ("Hi, mom.", " What are you doing?"), outcome.Returns)
 
 [<Fact>]
 let ``Zero or more`` () =
-    let p = Parser.Text.char 'a' |> Parser.zeroOrMore
-    let q = Parser.Text.char 'b' |> Parser.zeroOrMore
-    let parser = Parser.andAlso p q
-    let outcome = Parser.Text.run parser "aaabb"
+    let p = Parse.Text.char 'a' |> Parse.zeroOrMore
+    let q = Parse.Text.char 'b' |> Parse.zeroOrMore
+    let parser = Parse.andAlso p q
 
-    printfn "%A" outcome
-
+    let outcome = Parse.Text.run parser "aaabb"
     Assert.Equal(Some ([ 'a'; 'a'; 'a' ], ['b'; 'b']), outcome.Returns)
-    
-[<Fact>]
-let ``accept sequence`` () =
-    let a = Parser.Text.char 'a'
-    let b = Parser.Text.char 'b'
-    let c = Parser.Text.char 'c'
-    let p = Parser.andAlso a <| Parser.andAlso b c
 
-    let outcome = Parser.Text.run p "abcd"
+    let outcome = Parse.Text.run parser "aaa"
+    Assert.Equal(Some ([ 'a'; 'a'; 'a' ], []), outcome.Returns)
+
+    let outcome = Parse.Text.run parser "bb"
+    Assert.Equal(Some ([], ['b'; 'b']), outcome.Returns)
+
+[<Fact>]
+let ``Accept sequence`` () =
+    let a = Parse.Text.char 'a'
+    let b = Parse.Text.char 'b'
+    let c = Parse.Text.char 'c'
+    let p = Parse.andAlso a <| Parse.andAlso b c
+
+    let outcome = Parse.Text.run p "abcd"
+    printfn "%A" outcome
+    Assert.Equal(Some ('a', ('b', 'c')), outcome.Returns)
+
+    let outcome = Parse.Text.run p "abc"
+    printfn "%A" outcome
+    Assert.Equal(Some ('a', ('b', 'c')), outcome.Returns)
+
+    let outcome = Parse.Text.run p "ab"
+    printfn "%A" outcome
+    Assert.Equal(None, outcome.Returns)
+
+[<Fact>]
+let ``Builder`` () =
+    let abc = parse {
+        let! a = Parse.Text.char 'a'
+        let! b = Parse.Text.char 'b'
+        let! c = Parse.Text.char 'c'
+        return a, b, c
+    }
+
+    let outcome = Parse.Text.run abc "abcd"
     printfn "%A" outcome
 
-    Assert.Equal(Some ('a', ('b', 'c')), outcome.Returns)
+    Assert.Equal(Some ('a', 'b', 'c'), outcome.Returns)
+
+[<Fact>]
+let ``Separated sequences`` () =
+    let letter = Parse.accept Char.IsLetter
+    let comma = Parse.Text.char ','
+    let p = Parse.separatedBy comma letter
+
+    let outcome = Parse.Text.run p "a,b,c,d,e"
+    printfn "%A" outcome
+    Assert.Equal(Some ['a'; 'b'; 'c'; 'd'; 'e'], outcome.Returns)
+
+    let outcome = Parse.Text.run p "a,b,c,d,e,"
+    printfn "%A" outcome
+    Assert.Equal(Some ['a'; 'b'; 'c'; 'd'; 'e'], outcome.Returns)
+
+[<Fact>]
+let ``Enclosed within`` () =
+    let doubleQuote = Parse.Text.char '"'
+    let letters = 
+        Parse.accept Char.IsLetter
+        |> Parse.zeroOrMore
+        |> Parse.map String.Concat
+    let p = Parse.enclosedWithin doubleQuote doubleQuote letters
+
+    let outcome = Parse.Text.run p "\"Himom\""
+    printfn "%A" outcome
+    Assert.Equal(Some "Himom", outcome.Returns)
+
+    let outcome = Parse.Text.run p "\"\""
+    printfn "%A" outcome
+    Assert.Equal(Some "", outcome.Returns)
+
+[<Fact>]
+let ``Pick from any of the alternatives`` () =
+    let abc = 
+        Parse.anyOf ['a'; 'b'; 'c']
+        |> Parse.oneOrMore
+        |> Parse.map String.Concat
+
+    let outcome = Parse.Text.run abc "acbabcabc"
+    printfn "%A" outcome
+    Assert.Equal(Some "acbabcabc", outcome.Returns)
+
+    let abc = 
+        Parse.anyOf [| 'a'; 'b'; 'c' |]
+        |> Parse.oneOrMore
+        |> Parse.map String.Concat
+
+    let outcome = Parse.Text.run abc "b"
+    printfn "%A" outcome
+    Assert.Equal(Some "b", outcome.Returns)
+
+[<Fact>]
+let ``Choices`` () =
+    let abc = parse {
+        let! abcs = 
+            [ Parse.Text.char 'a'
+              Parse.Text.char 'b'
+              Parse.Text.char 'c' ]
+            |> Parse.choice
+            |> Parse.zeroOrMore
+
+        return String.Concat abcs
+    }
+
+    let outcome = Parse.Text.run abc "acbabcabc"
+    printfn "%A" outcome
+    Assert.Equal(Some "acbabcabc", outcome.Returns)
--- Combinatrix/src/Combine.fs
+++ Combinatrix/src/Combine.fs
@@ -1,27 +1,26 @@
 module Combinatrix.Combine
 
 (* If I wrap this, then I can have extension methods on it. *)
-type ('t, 'a) Parser = 't ParseState -> ParseResult<'t, 'a>
+type ('t, 'a) Parse = 
+    't ParseState -> ParseResult<'t, 'a>
 
 and 't ParseState =
-    { Input  : 't []
-      TokenStart : int 
+    { Input       : 't []
+      TokenStart  : int 
       TokenLength : int }
 
 and ('t, 'a) ParseResult =
     { State   : 't ParseState
       Returns : 'a option }
 
-and Fault = Expected of string 
-          | Eof
-
 module ParseState =
     let from input =
-        { Input  = input
-          TokenStart = 0
+        { Input       = input
+          TokenStart  = 0
           TokenLength = 0 }
 
-    let offset { TokenStart = start; TokenLength = length } =
+    let offset { TokenStart  = start
+                 TokenLength = length } =
         start + length
 
     let peek self =
@@ -47,21 +46,23 @@ module ParseResult =
         { State   = input
           Returns = None }
 
-    let map f { State = input; Returns = returns } =
+    let map f { State   = input
+                Returns = returns } =
         { State   = input
           Returns = Option.map f returns }
 
-    let bind f { State = input; Returns = returns } : ParseResult<'t, 'b> =
+    let bind f { State   = input
+                 Returns = returns } : ParseResult<'t, 'b> =
         f input returns
 
-module Parser =
-    let run (self : Parser<'t, 'a>) =
+module Parse =
+    let run (self : Parse<'t, 'a>) =
         ParseState.from >> self
 
-    let empty returns : Parser<'t, 'a> =
+    let empty returns : Parse<'t, 'a> =
         ParseResult.accepted returns
 
-    let take count : Parser<'t, 't []> = fun input ->
+    let take count : Parse<'t, 't []> = fun input ->
         if ParseState.canAdvance count input then
             let input = ParseState.advance count input
             let token = ParseState.token input
@@ -69,7 +70,7 @@ module Parser =
         else
             ParseResult.balked input
 
-    let accept suchThat : Parser<'t, 't> = fun input ->
+    let accept suchThat : Parse<'t, 't> = fun input ->
         if ParseState.canAdvance 1 input && ParseState.peek input |> suchThat then
             let input = ParseState.advance 1 input
             let token = ParseState.token input
@@ -77,33 +78,37 @@ module Parser =
         else
             ParseResult.balked input
 
-    let map f self : Parser<'t, 'b> =
+    let map f self : Parse<'t, 'b> =
         self >> ParseResult.map f
 
-    let filterMap f self : Parser<'t, 'b> =
+    let filterMap f self : Parse<'t, 'b> =
         self
         >> ParseResult.map f
-        >> function { State = input; Returns = Some (Some x) } -> ParseResult.accepted x input
-                  | { State = input }                          -> ParseResult.balked input
+        >> function { State   = input
+                      Returns = Some (Some x) } -> ParseResult.accepted x input
+                  | { State = input }           -> ParseResult.balked input
 
-    let bind f self : Parser<'t, 'b> =
+    let bind f self : Parse<'t, 'b> =
          self
          >> ParseResult.map f
-         >> function { State = input; Returns = Some q } -> q input
-                   | { State = input }                   -> ParseResult.balked input
+         >> function { State   = input
+                       Returns = Some q } -> q input
+                   | { State = input }    -> ParseResult.balked input
 
-    let orElse (p : Parser<'t, 'a>) (q : Parser<'t, 'a>) : Parser<'t, 'a> =
+    let orElse (p : Parse<'t, 'a>) (q : Parse<'t, 'a>) : Parse<'t, 'a> =
         p >> function { Returns = Some _ } as it -> it
                     | { State = input }          -> q input
+
+    let choice alternatives : Parse<'t, 'a> =
+        Seq.reduce orElse alternatives
 
     let andAlso p q =
         bind (fun px -> map (fun qx -> (px, qx)) q) p
 
-    let rec zeroOrMore (self : Parser<'t, 'a>) : Parser<'t, 'a list> =
+    let rec zeroOrMore (self : Parse<'t, 'a>) : Parse<'t, 'a list> =
         let iterate = 
             function [] -> empty []
                    | xs -> zeroOrMore self |> map (List.append xs)
-        // Is this as reducesd as it can be?
         empty []
         |> orElse (map List.singleton self)
         |> bind iterate
@@ -137,15 +142,26 @@ module Parser =
         accept (fun tok -> Seq.contains tok alternatives)
 
     module Text =
-        let literal (text : string) : Parser<char, string> =
+        let literal (text : string) : Parse<char, string> =
             let textChars = text.ToCharArray ()
             take text.Length
             |> filterMap (fun t -> if t = textChars then Some text else None)
 
-        let char ch : Parser<char, char> =
+        let char ch : Parse<char, char> =
             accept (fun tok -> tok = ch)
 
-        let run (parser : Parser<char, 'a>) (input : string) =
+        let run (parser : Parse<char, 'a>) (input : string) =
             input.ToCharArray ()
             |> run parser
-    
+
+    type Builder () =
+        member __.Bind (self, f) =
+            bind f self
+
+        member __.Return (x) =
+            empty x
+
+        member __.ReturnFrom (self) =
+            self
+
+let parse = new Parse.Builder ()
